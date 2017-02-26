ca9a33a33d0620e7b62eb8391b6ebc0c6b590e7a
LucianBor47 <sniddly@hotmail.com>
2017-02-26 14:06:39 -0500

    Added rounding to thrid decimal in arrival.php

--- sem2/arrival.php
+++ sem2/arrival.php
@@ -39,7 +39,8 @@
     </div> 
     <div>
     <p>The amount of time it will take you to travel <?php echo $distance ?> miles at <?php echo $speed ?> miles per hour is:</p>
-        <p><?php echo round($time,2) ?> hours</p>
+        <p><?php echo round($time,2)." rounded to two decimals."?> hours</p>
+        <p><?php echo round($time,3)." rounded to three decimals."?> hours</p>
     </div>
 </body>
 </html>
